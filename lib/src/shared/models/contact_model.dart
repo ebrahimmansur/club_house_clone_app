b2d14ee9d352a9a6902f53608677d3e23413310b
Ebrahim <ebrahimmansur90117@gmail.com>
2022-05-22 21:39:22 +0200

    🌱 add contact model

---
+++ lib/src/shared/models/contact_model.dart
@@ -0,0 +1,13 @@
+class ContactModel {
+  final int id;
+  final String name;
+  final String handlerName;
+  final String profileImageUrl;
+
+  ContactModel({
+    required this.id,
+    required this.name,
+    required this.handlerName,
+    required this.profileImageUrl,
+  });
+}
