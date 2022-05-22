c8ab1aafaa1a7aeeebeffb605b3dbe6f79574048
Ebrahim <ebrahimmansur90117@gmail.com>
2022-05-22 21:38:36 +0200

    🌱 add profle model

---
+++ lib/src/shared/models/profile_model.dart
@@ -0,0 +1,10 @@
+class ProfileModel {
+  final int id;
+  final String fullName;
+  final String profileImageUrl;
+
+  ProfileModel(
+      {required this.id,
+      required this.fullName,
+      required this.profileImageUrl});
+}
