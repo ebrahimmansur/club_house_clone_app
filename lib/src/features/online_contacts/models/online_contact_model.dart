34942b545c433353c23588aa9c01cc38c2cd80d3
Ebrahim <ebrahimmansur90117@gmail.com>
2022-05-23 23:19:37 +0200

    🎨 add online contact model

---
+++ lib/src/features/online_contacts/models/online_contact_model.dart
@@ -0,0 +1,12 @@
+class OnlineContactModel {
+  final int id;
+  final String name;
+  final String handler;
+  final String profileImageUrl;
+  OnlineContactModel({
+    required this.id,
+    required this.name,
+    required this.handler,
+    required this.profileImageUrl,
+  });
+}
