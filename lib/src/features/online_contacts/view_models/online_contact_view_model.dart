948479464fe34f3a09c8740e820321cd68123306
Ebrahim <ebrahimmansur90117@gmail.com>
2022-05-24 00:09:53 +0200

    🌱 add online contact view model

---
+++ lib/src/features/online_contacts/view_models/online_contact_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnlineContactViewModel {
+  final int id;
+  final String name;
+  final Color onlineColor;
+  final String imageUrl;
+  OnlineContactViewModel({
+    required this.id,
+    required this.name,
+    required this.onlineColor,
+    required this.imageUrl,
+  });
+}
