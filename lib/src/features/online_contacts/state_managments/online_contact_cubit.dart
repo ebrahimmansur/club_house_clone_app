18976c741b5195a9966f429a32ca656b22d070ee
Ebrahim <ebrahimmansur90117@gmail.com>
2022-05-24 00:09:34 +0200

    ⬆️ update online contact state
     - update states
     - update cubit

--- lib/src/features/online_contacts/state_managments/online_contact_cubit.dart
+++ lib/src/features/online_contacts/state_managments/online_contact_cubit.dart
@@ -1,8 +1,21 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
+import 'dart:async';
+import 'package:club_house_clone_app/src/features/online_contacts/mappers/online_contact_failure_mapper.dart';
+import 'package:club_house_clone_app/src/shared/abstractions/i_mapper.dart';
+import '../../../shared/shared.dart';
+import '../online_contacts.dart';
 part 'online_contact_state.dart';
 
 class OnlineContactCubit extends Cubit<OnlineContactState> {
+  final IOnlineContactService _onlineContactService = OnlineContactService();
+  final IMapper _onlineContactViewMapper = OnlineContactViewMapper();
+  final IMapper _onlineContactFailureMapper = OnlineContactFailureMapper();
   OnlineContactCubit() : super(OnlineContactInitial());
+  FutureOr getOnlineContacts() async {
+    emit(LoadingOnlineContactState());
+    final result = await _onlineContactService.getOnlineContacts();
+    result.result(
+        (BaseFailure failure) => emit(_onlineContactFailureMapper.map(failure)),
+        (List<OnlineContactModel> data) => emit(SucessOnlineContactState(
+            models: _onlineContactViewMapper.map(data))));
+  }
 }
--- lib/src/features/online_contacts/state_managments/online_contact_state.dart
+++ lib/src/features/online_contacts/state_managments/online_contact_state.dart
@@ -8,3 +8,25 @@ abstract class OnlineContactState extends Equatable {
 }
 
 class OnlineContactInitial extends OnlineContactState {}
+
+class LoadingOnlineContactState extends OnlineContactState {}
+
+class NetworkFailureOnlineContactState extends OnlineContactState {
+  final String message;
+  const NetworkFailureOnlineContactState({required this.message});
+}
+
+class ServerFailureOnlineContactState extends OnlineContactState {
+  final String message;
+  const ServerFailureOnlineContactState({required this.message});
+}
+
+class UnknownFailureOnlineContactState extends OnlineContactState {
+  final String message;
+  const UnknownFailureOnlineContactState({required this.message});
+}
+
+class SucessOnlineContactState extends OnlineContactState {
+  final List<OnlineContactViewModel> models;
+  const SucessOnlineContactState({required this.models});
+}
