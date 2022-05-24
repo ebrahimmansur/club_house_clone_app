0154655dd05c1fcc9d3fe4bc5c9fe2ea10c1ae58
Ebrahim <ebrahimmansur90117@gmail.com>
2022-05-24 20:34:42 +0200

    🌱 add home screen

---
+++ lib/src/features/home/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
