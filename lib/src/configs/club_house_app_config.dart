a764186b58eab3363f67dabeacdfbac746d46da7
Ebrahim <ebrahimmansur90117@gmail.com>
2022-06-10 12:36:58 +0200

    ♻️ update clube house app config
     - add system ui overlay style

--- lib/src/configs/club_house_app_config.dart
+++ lib/src/configs/club_house_app_config.dart
@@ -1,21 +1,36 @@
 import 'dart:async';
 
-import 'package:club_house_clone_app/src/core/i_app_host_builder.dart';
-import 'package:club_house_clone_app/src/features/home/screens/home_screen.dart';
+import 'package:club_house_clone_app/src/club_house_app.dart';
+import 'package:club_house_clone_app/src/core/app_hosts/i_app_host_builder.dart';
+import 'package:club_house_clone_app/src/shared/shared.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ClubHouseAppConfig implements IAppHostBuilder {
   @override
   Widget build() {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    return const ClubHouseApp();
   }
 
   @override
-  FutureOr runConfig() {
-    debugPrint("No Config found");
+  FutureOr runConfig() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    if (_isDark) {
+      _enableDarkSystemUiOverlayStyle();
+    } else {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+          statusBarColor: ColorResource.primeryBackgroundColor,
+          statusBarIconBrightness: Brightness.light));
+    }
   }
+
+  void _enableDarkSystemUiOverlayStyle() {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: ColorResource.primeryDarkBackgroundColor,
+        statusBarIconBrightness: Brightness.light));
+  }
+
+  bool get _isDark => false;
 }
